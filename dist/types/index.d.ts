541a2c3faa7fe6967dc75baa6cb366dc542647fd
Viktor <code@viktorchin.nl>
2023-10-28 16:02:16 +0200

    Update distributables

--- dist/types/index.d.ts
+++ dist/types/index.d.ts
@@ -1,11 +1,11 @@
-export { default as createEl } from "./createEl";
-export { default as createEls } from "./createEls";
-export { default as cssTimeToMs } from "./cssTimeToMs";
-export { default as fetchWithTimeout } from "./fetchWithTimeout";
-export { default as getAverage } from "./getAverage";
-export { default as getCssUnit } from "./getCssUnit";
-export { default as getElPropValue } from "./getElPropValue";
-export { default as getPseudoRandomIntBelow } from "./getPseudoRandomIntBelow";
-export { default as isMotionAllowed } from "./isMotionAllowed";
-export { default as sleep } from "./sleep";
+export { default as createEl } from "./createEl.js";
+export { default as createEls } from "./createEls.js";
+export { default as cssTimeToMs } from "./cssTimeToMs.js";
+export { default as fetchWithTimeout } from "./fetchWithTimeout.js";
+export { default as getAverage } from "./getAverage.js";
+export { default as getCssUnit } from "./getCssUnit.js";
+export { default as getElPropValue } from "./getElPropValue.js";
+export { default as getPseudoRandomIntBelow } from "./getPseudoRandomIntBelow.js";
+export { default as isMotionAllowed } from "./isMotionAllowed.js";
+export { default as sleep } from "./sleep.js";
 //# sourceMappingURL=index.d.ts.map
