f572f5616b809218d847293e92d37c94ecde0f7d
Viktor <code@viktorchin.nl>
2023-11-08 02:30:07 +0100

    Add tests for getAverage()

---
+++ tests/getAverage.test.ts
@@ -0,0 +1,18 @@
+import { expect, test } from "@jest/globals";
+import getAverage from "../src/getAverage.ts";
+
+test("Get average of an array of numbers", () => {
+    expect(getAverage([0, 1, 5, 8, 10])).toBeCloseTo(4.8);
+    expect(getAverage([0.1, 0.44, 0.9])).toBeCloseTo(0.48);
+    expect(getAverage([0])).toBe(0);
+    expect(getAverage([-1, -9, 0.00001, 100000])).toBeCloseTo(24997.5);
+    expect(getAverage([-1, -9, 0.00001, 100000], "floor")).toBe(24997);
+    expect(getAverage([-1, -9, 0.00001, 100000], "ceil")).toBe(24998);
+
+    expect(() => getAverage(null as unknown as number[])).toThrowError();
+    expect(() => getAverage(false as unknown as number[])).toThrowError();
+    expect(getAverage(["75.10", "88.88", "100"] as unknown as number[])).toBeNaN();
+    expect(getAverage(["75.10", "88,88", "100"] as unknown as number[])).toBeNaN();
+    expect(getAverage(["75.10", "88.88", 100] as unknown as number[])).toBeNaN();
+    expect(getAverage(["75.10kHz"] as unknown as number[])).toBeNaN();
+});
