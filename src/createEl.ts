d57c743bed893d42febbf89d539f30becb485723
Viktor <code@viktorchin.nl>
2023-11-24 12:36:09 +0100

    Improve createEl attr value type handling

--- src/createEl.ts
+++ src/createEl.ts
@@ -1,11 +1,22 @@
-type ElAttrs = { [key: string]: string };
+type ElAttrs = { [key: string]: string | number | boolean | null };
 type ElTagName = string;
 
 function createEl(tagName: ElTagName, attrs?: ElAttrs | undefined) {
     const el = document.createElement(tagName);
 
     if (attrs) {
-        for (const [prop, val] of Object.entries(attrs)) {
+        for (const [prop, rawVal] of Object.entries(attrs)) {
+            if (
+                (typeof rawVal !== "string" &&
+                    typeof rawVal !== "number" &&
+                    typeof rawVal !== "boolean") ||
+                rawVal === false
+            )
+                continue;
+
+            const val =
+                typeof rawVal === "number" || typeof rawVal === "boolean" ? String(rawVal) : rawVal;
+
             if (prop === "text") {
                 el.textContent = val;
 
