344433943a08ea261a07a543686a42893002e9c5
Viktor <code@viktorchin.nl>
2024-06-14 20:26:39 +0200

    test(cssDurationToMs): Minor test improvements

--- tests/cssDurationToMs.test.ts
+++ tests/cssDurationToMs.test.ts
@@ -1,14 +1,14 @@
 import { expect, test } from "@jest/globals";
 import cssDurationToMs from "../src/cssDurationToMs.ts";
 
-test("Valid time type, valid duration", () => {
+test("Valid duration type", () => {
     expect(cssDurationToMs("10ms")).toBe(10);
-    expect(cssDurationToMs("100s")).toBe(100000);
+    expect(cssDurationToMs("0.322s")).toBe(322);
+    expect(cssDurationToMs("55s")).toBe(55000);
     expect(cssDurationToMs("1m")).toBe(60000);
     expect(cssDurationToMs("2h")).toBe(7200000);
-    expect(cssDurationToMs("4d")).toBe(345600000);
-    expect(cssDurationToMs("3w")).toBe(1814400000);
     expect(cssDurationToMs("0.5d")).toBe(43200000);
+    expect(cssDurationToMs("-76ms")).toBe(-76);
     expect(cssDurationToMs("-.5m")).toBe(-30000);
     expect(cssDurationToMs("-1w")).toBe(-604800000);
 });
