253c7adc26f05a23a57c7a995beff0808e900544
Viktor <code@viktorchin.nl>
2025-02-26 19:14:13 +0100

    perf(fetchWithTimeout): Refer to options object instead of creating a deep clone

--- src/fetchWithTimeout.ts
+++ src/fetchWithTimeout.ts
@@ -6,8 +6,9 @@ async function fetchWithTimeout(
     if (typeof timeoutDuration !== "number")
         throw new Error("`timeoutDuration` must be a `Number`.");
 
+    const options = fetchOptions || {};
+
     const ac = new AbortController();
-    const options = fetchOptions ? Object.assign({}, fetchOptions) : {};
     options.signal = ac.signal;
     const timeoutId = setTimeout(() => ac.abort(), timeoutDuration || 8000);
 
