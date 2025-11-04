3db67090d7b272d4bf38993c63754f537264816d
Viktor <code@viktorchin.nl>
2025-11-04 01:19:28 +0100

    feat(getAverage): Add Math.round rounding

--- src/getAverage.ts
+++ src/getAverage.ts
@@ -1,4 +1,4 @@
-function getAverage(numbers: number[], round?: "floor" | "ceil") {
+function getAverage(numbers: number[], round?: "nearest" | "floor" | "ceil") {
     if (!Array.isArray(numbers) || numbers.length === 0)
         throw new Error("`numbers` must be a non-empty array.");
     if (round && typeof round !== "string") throw new Error("`round` value is invalid.");
@@ -8,6 +8,9 @@ function getAverage(numbers: number[], round?: "floor" | "ceil") {
     if (Number.isNaN(average)) throw new Error("Each item in `numbers` must be a `number`.");
 
     switch (round) {
+        case "nearest":
+            return Math.round(average);
+
         case "floor":
             return Math.floor(average);
 
--- test/getAverage.test.ts
+++ test/getAverage.test.ts
@@ -7,6 +7,7 @@ test("Valid input items", () => {
     expect(getAverage([0.1, 0.44, 0.9])).toBeCloseTo(0.48);
     expect(getAverage([-104, -52, -87])).toBe(-81);
     expect(getAverage([-1, -9, 0.00001, 100000])).toBeCloseTo(24997.5);
+    expect(getAverage([-1, -9, 0.00001, 100000], "nearest")).toBe(24998);
     expect(getAverage([-1, -9, 0.00001, 100000], "floor")).toBe(24997);
     expect(getAverage([-1, -9, 0.00001, 100000], "ceil")).toBe(24998);
 });
